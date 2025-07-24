a4d7005012af66577e735f7ff95477d884bededc
Abhishek-223 <abhishek223official@gmail.com>
2025-07-24 22:06:48 +0530

    apiResponse and asyncHandler updated

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 // promise code of asyncHandler
 const asyncHandler = (requestHandler) => {
-    (req,res,next) =>{
+    return (req,res,next) =>{
         Promise.resolve(requestHandler(req,res,next)).
         catch((err) => next(err))
     }
